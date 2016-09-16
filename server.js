da352d67d5c4eac9a2d3d76d1053ce2a1559ffaa
Vince Suriani <vince@Vinces-MacBook-Pro.local>
2016-09-16 13:40:52 -0400

    how do I do dynamic routing

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//$ node server.js
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -10,4 +12,23 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/www/index.html'));
 });
 
+app.use('/imgs', express.static(__dirname + '/www/imgs'));
+app.use('/js', express.static(__dirname + '/www/css'));
+app.use('/css', express.static(__dirname + '/www/js'));
+
+app.use(function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/www/404.html'));
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(thePort);
+
+
+
+
+
+console.log("server running...");
